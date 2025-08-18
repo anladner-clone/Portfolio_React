302a4920c1984908e56d5f87ecac0ab14bc01826
Ariel <ari2006ladner@gmail.com>
2025-08-18 18:00:01 -0300

    Commit arreglando carousel.jsx

--- src/components/Organisms/Carousel.jsx
+++ src/components/Organisms/Carousel.jsx
@@ -6,17 +6,9 @@ export const Carousel = ({ children }) => {
     <div className={styles.carousel__container}>
 
       <div>
-        {Array.isArray(children)
-          ? children.map((child, i) => (
-            <div key={child?.key ?? i}>
-              {child}
-            </div>
-          ))
-          : (
-            <div>
-              {children}
-            </div>
-          )}
+
+        {children}
+
       </div>
     </div>
   );
